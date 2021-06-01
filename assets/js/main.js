8dfa5ce23fe6a4b5d12fcc8f082a24df84a2c894
Adam <adamrzymski@gmail.com>
2021-06-01 11:51:51 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+class Track {
+  constructor(colours) {
+    this.track = document.getElementById("track");
+    this.btn = document.getElementById("start");
+    this.colours = colours;
+    this.horses = [];
+    this.place = 0;
+  }
+  init() {
+    this.colours.forEach((c) => this.addHorse(new Horse(c)));
+  }
+  reset() {
+    this.horses = [];
+    this.track.innerHTML = "";
+    this.init();
+  }
+  addHorse(horse) {
+    this.horses.push(horse);
+    this.track.appendChild(horse.box);
+  }
+  startRace() {
+    this.place = 0;
+    this.horses.forEach((h) => h.start());
+  }
+}
+
+class Horse {
+  constructor(colour) {
+    this.box = document.createElement("div");
+    this.img = document.createElement("img");
+    this.img.src = "./assets/img/horse.png";
+    this.box.style.backgroundColor = colour;
+    this.box.classList.add("horse");
+    this.box.appendChild(this.img);
+    this.x = 0;
+  }
+
+  start() {
+    const move = () => {
+      this.x += Math.random() * 10;
+      this.x = Math.floor(this.x);
+      this.box.style.left = this.x + "px";
+      if (this.x < 900) requestAnimationFrame(move);
+      else this.box.dataset.position = ++track.place;
+      if (track.place == track.colours.length) track.btn.innerHTML = "Reset";
+    };
+    move();
+  }
+}
+
+const action = (btn) => {
+  if (btn.innerHTML == "Go") track.startRace();
+  if (btn.innerHTML == "Reset") {
+    btn.innerHTML = "Go";
+    track.reset();
+  }
+};
+
+const colours = ["red", "orange", "yellow", "white", "blue", "indigo", "violet", "black", "brown", "pink", "azure"];
+const track = new Track(colours);
+track.init();
